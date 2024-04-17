fdf542e9105c0180cc30662435e0517b04bbf5b0
NateDev <jonathanmbaya13@gmail.com>
2024-04-17 02:18:58 +0200

    Jeu opérationnel

--- main.py
+++ main.py
@@ -1,34 +1,85 @@
 import tkinter
 
+def check_nul():
+    print("Match nul")
+
+def print_winner():
+    global win
+
+    if win is False:
+        win = True
+        print("Le joueur", current_player, "a gagné le jeu")
+
+def switcher_player():
+    global current_player
+    if current_player == 'X':
+        current_player = '0'
+    else:
+        current_player = 'X'
+
 def check_win( clicked_row, clicked_col):
     #detecter la victoire horizontale
     count = 0
     for i in range(3):
         current_button = buttons[i][clicked_row]
-        if current_button['text']== "X":
+        if current_button['text']== current_player:
             count+= 1
     
     if count == 3:
-        print("Victoire !")
-
+        print_winner()
 
     #detecter la victoire verticale
     count = 0
     for i in range(3):
-        current_button = buttons[i][clicked_row]
-        if current_button['text']== "X":
+        current_button = buttons[clicked_col][i]
+        if current_button['text']== current_player:
+            count+= 1
+
+    if count == 3:
+        print_winner()
+
+
+    #detecter la victoire diagonale
+    count = 0
+    for i in range(3):
+        current_button = buttons[i][i]
+        if current_button['text']== current_player:
             count+= 1
     
     if count == 3:
-        print("Victoire !")
+        print_winner()
+
+
+    #detecter la victoire diagonale inversé
+    count = 0
+    for i in range(3):
+        current_button = buttons[2-i][i]
+        if current_button['text']== current_player:
+            count+= 1
+    
+    if count == 3:
+        print_winner()
+    
+    if win is False:
+        count = 0
+        for col in range(3):
+            for row in range(3):
+                current_button = buttons[col][row]
+                if current_button['text'] == 'X' or current_button['text'] == '0':
+                    count += 1
+        print(count)
+        if count == 9:
+            print ("Match nul")
+    
+
 
 def place_symbol(row, column):
-    print('click', row, column)
 
     clicked_button = buttons[column][row]
-    clicked_button.config(text="X")
-
-    check_win(row, column)
+    if clicked_button['text'] == "":
+        clicked_button.config(text=current_player)
+        check_win(row, column)
+        switcher_player()
 
 def draw_grid():
     for column in range(3):
@@ -45,6 +96,8 @@ def draw_grid():
 
 # stockage
 buttons = []
+current_player = 'X'
+win = False
 
 # Créer la fenêtre de jeu 
 root = tkinter.Tk()
